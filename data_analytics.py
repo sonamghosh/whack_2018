e86d87088063f2591a6d568ac5f6417de84a6503
Sonam Ghosh <sonamghosh@gmail.comu>
2018-11-04 01:57:30 -0400

    Added graphing for high low close plots

--- data_analytics.py
+++ data_analytics.py
@@ -1,3 +1,4 @@
+
 import numpy as np 
 import pandas as pd 
 
@@ -26,20 +27,26 @@ def plot_and_save_plot(ticker, source):
         d = get_quote_data(ticker, '1y', '1d')
         # Remove timestamp and keep only dates
         d.index = d.index.normalize()
-        print(d.columns)
-        d2 = d[['CLOSE']]
-        d2 = d2.reset_index()
         #print(d2)
-        g = sns.relplot(x='Datetime', y='CLOSE', kind='line', data=d2)
+        #fig, ax = plt.subplots()
+        #g = sns.relplot(x='Datetime', y='CLOSE', kind='line', data=d2)
+        wide_df = d[['CLOSE', 'LOW', 'HIGH']]
+        #wide_df = wide_df.reset_index()
+        ax = sns.lineplot(data=wide_df)
         plt.xlabel('Time')
         plt.ylabel('Value')
         plt.title('Stock price of ' + ticker + ' from 11-06-2017 to 11-02-2018')
+        plt.savefig(ticker+'_graph.png')
         plt.show()
 
 
 
 
 if __name__ == "__main__":
-    tickers = ['TSLA', 'MSFT', 'AAPL', 'GOOG', 'NFLX', 'TRIP', 'AMZN']
-    plot_and_save_plot('TSLA', 'yahoo')
+    tickers = ['TSLA', 'MSFT', 'AAPL', 'GOOG', 'NFLX', 'TRIP', 'AMZN', 'FTQGX']
+    plot_and_save_plot('FTQGX', 'yahoo')
+    #fmri = sns.load_dataset("fmri")
+    #print(fmri)
+    #ax = sns.lineplot(x="timepoint", y="signal", data=fmri)
+    #plt.show()
 
